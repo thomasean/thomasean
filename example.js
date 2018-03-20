e5ab8d68ef965952f3ea111fd505a84e14c90904
Sean Thomas <sthomas@sthomas-mac.local>
2018-03-20 09:32:44 -0400

    example js

--- example.js
+++ example.js
@@ -1,4 +1,27 @@
-var a = 5;
-var b = 10;
-console.log(`Fifteen is ${a + b} and 
-not ${2 * a + b}.`);
+var person = 'Sean';
+var age = 28;
+
+function myTag(strings, personExp, ageExp) {
+
+  var str0 = strings[0];
+  var str1 = strings[1];
+
+  // There is technically a string after
+  // the final expression (in our example),
+  // but it is empty (""), so disregard.
+  // var str2 = strings[2];
+
+  var ageStr;
+  if (ageExp > 99){
+    ageStr = 'centenarian';
+  } else {
+    ageStr = 'youngster';
+  }
+
+  return str0 + personExp + str1 + ageStr;
+
+}
+
+var output = myTag`that ${ person } is a ${ age }`;
+
+console.log(output);
