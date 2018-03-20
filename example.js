1f13b179b9e0db87e1c6dce59675d63dca9185ee
Sean Thomas <sthomas@sthomas-mac.local>
2018-03-20 09:36:21 -0400

    try to rebuild

--- example.js
+++ example.js
@@ -3,8 +3,8 @@ var age = 28;
 
 function myTag(strings, personExp, ageExp) {
 
-  var str0 = strings[0];
-  var str1 = strings[1];
+  var str0 = strings[0]; // "that "
+  var str1 = strings[1]; // " is a "
 
   // There is technically a string after
   // the final expression (in our example),
@@ -25,3 +25,4 @@ function myTag(strings, personExp, ageExp) {
 var output = myTag`that ${ person } is a ${ age }`;
 
 console.log(output);
+// that Sean is a youngster
