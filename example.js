614a7bc97c9e46b668f722c481bc65912eef6a53
Sean Thomas <columbuscoder@gmail.com>
2019-04-27 21:53:16 -0400

    Just clearing stuff.

--- example.js
+++ example.js
@@ -1,10 +1,10 @@
-const accordions = document.querySelectorAll("[data-accordion]");
+// const accordions = document.querySelectorAll("[data-accordion]");
 
-accordions.forEach(function(accordion){
-	accordion.addEventListener('click', function(e) {
-		var parNode = e.target.parentNode;
-		if (parNode.className = "got--accordion__item") {
-			parNode.classList.toggle("got--accordion__item--active");
-		}
-	})
-});
+// accordions.forEach(function(accordion){
+// 	accordion.addEventListener('click', function(e) {
+// 		var parNode = e.target.parentNode;
+// 		if (parNode.className = "got--accordion__item") {
+// 			parNode.classList.toggle("got--accordion__item--active");
+// 		}
+// 	})
+// });
